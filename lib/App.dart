ea397449029cee574f0d1043e2b7aa29e46397cb
Kryštof Woldřich <krystof51@gmail.com>
2019-04-15 21:59:11 +0200

    add routing of bottom navigation bar
    correct test
    
    
    test edit

---
+++ lib/App.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import './routes/MainRoutes.dart';
+
+class MyApp extends StatelessWidget {
+  final applicationTitle = 'Museum Guide';
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    final routes = {
+      MainRoutes.dashboard: _buildDashboard,
+      MainRoutes.tickets: _buildTickets,
+      MainRoutes.map: _buildMap,
+      MainRoutes.search: _buildSearch,
+    };
+
+    return MaterialApp(
+      title: applicationTitle,
+      initialRoute: MainRoutes.dashboard,
+      routes: routes,
+    );
+  }
+
+  Widget _buildDashboard(BuildContext context) {
+    throw Exception('Method not implemented.');
+  }
+
+  Widget _buildTickets(BuildContext context) {
+    throw Exception('Method not implemented.');
+  }
+
+  Widget _buildMap(BuildContext context) {
+    throw Exception('Method not implemented.');
+  }
+
+  Widget _buildSearch(BuildContext context) {
+    throw Exception('Method not implemented.');
+  }
+}
