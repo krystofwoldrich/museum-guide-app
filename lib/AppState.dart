7e92912b40c985089fd6fb7c639f464991d8afa1
Kryštof Woldřich <krystof51@gmail.com>
2019-04-21 13:45:19 +0200

    add tickets to redux

--- lib/AppState.dart
+++ lib/AppState.dart
@@ -1,4 +1,25 @@
-class AppState {
+import 'package:meta/meta.dart';
+import './model/Ticket.dart';
 
-  AppState();
+@immutable
+class AppState {
+  final List<Ticket> tickets;
+
+  AppState({
+    @required List<Ticket> tickets
+  }) :
+    this.tickets = List.unmodifiable(tickets);
 }
+
+final initialState = new AppState(
+  tickets: [Ticket(
+    name: 'trs',
+    description: 'adda',
+    price: 12.88,
+  ),
+  Ticket(
+    name: 'trsss',
+    description: 'adda',
+    price: 12.88,
+  )]
+);
