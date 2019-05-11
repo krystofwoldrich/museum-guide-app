d136affca06d9e98e1e971d90e800b91fa222d60
Kryštof Woldřich <krystof51@gmail.com>
2019-05-11 11:39:55 +0200

    add open sans font

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 final ThemeData customThemeData =
   ThemeData(
+    fontFamily: 'OpenSans',
     brightness: Brightness.light,
     primaryColor: Color.fromRGBO(128, 0, 0, 1),
     backgroundColor: Color.fromRGBO(240, 240, 240, 1)
