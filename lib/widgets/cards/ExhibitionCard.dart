9cac8f78c918824c173955559c86e16305646afd
Kryštof Woldřich <krystof51@gmail.com>
2019-05-11 12:17:51 +0200

    add exhibition card design
    MOCK background image

--- lib/widgets/cards/ExhibitionCard.dart
+++ lib/widgets/cards/ExhibitionCard.dart
@@ -26,19 +26,38 @@ class ExhibitionCard extends StatelessWidget {
     ];
 
     if (this.description != null) {
-      content.add(Text(
-        this.description,
-        style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400)
+      content.add(Container(
+        child: Text(
+          this.description,
+          style: TextStyle(fontSize: 14, fontWeight: FontWeight.w400)
+        ),
+        margin: EdgeInsets.only(top: 16),
       ));
     }
 
-    return Container(
-      width: 200,
+    return this._withCustomTheme(context, Container(
       child: Card(
         child: InkWell(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: content,
+          child: Container(
+            child: Column(
+              children: content,
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+            ),
+            padding: EdgeInsets.only(
+              left: 16,
+              top: 16,
+              right: 24,
+              bottom: 30,
+            ),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage('https://whitney.org/uploads/image/file/822181/Skull_red.jpg'),
+                fit: BoxFit.cover,
+                colorFilter: ColorFilter.mode(Color.fromRGBO(66, 66, 66, 0.68), BlendMode.srcOver),
+              ),
+            ),
           ),
           onTap: () {
             Navigator.of(context).push(MaterialPageRoute(
@@ -47,6 +66,19 @@ class ExhibitionCard extends StatelessWidget {
           },
         ),
       ),
-    );
+    ));
+  }
+
+  Widget _withCustomTheme(BuildContext context, Widget widget) {
+    return Theme(
+      data: Theme.of(context).copyWith(
+        textTheme: TextTheme(
+          body1: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+      child: widget,
+    );  
   }
 }
