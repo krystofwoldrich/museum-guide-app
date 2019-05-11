2d027943437bf8c281525bb8d12b919e70405580
Kryštof Woldřich <krystof51@gmail.com>
2019-05-11 11:53:40 +0200

    add design colors

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
+final Color _colorGrey = Color.fromRGBO(240, 240, 240, 1);
+final Color _colorDarkGrey = Color.fromRGBO(64, 64, 64, 1);
+final Color _colorRed = Color.fromRGBO(128, 0, 0, 1);
+
 final ThemeData customThemeData =
   ThemeData(
     fontFamily: 'OpenSans',
     brightness: Brightness.light,
-    primaryColor: Color.fromRGBO(128, 0, 0, 1),
-    backgroundColor: Color.fromRGBO(240, 240, 240, 1)
+    primaryColor: _colorRed,
+    accentColor: _colorRed,
+    canvasColor: _colorGrey,
+    appBarTheme: AppBarTheme(
+      brightness: Brightness.dark,
+      color: _colorDarkGrey,
+      textTheme: TextTheme(
+        body1: TextStyle(
+          color: _colorDarkGrey,
+        ),
+      ),
+    ),
+    textTheme: TextTheme(
+      body1: TextStyle(
+        color: _colorDarkGrey
+      ),
+    ),
   );
