6d0152aededccdc1ae3aca2ddf2d72305e9e522a
Kryštof Woldřich <krystof51@gmail.com>
2019-05-13 23:41:43 +0200

    add auto size text to the titles widgets

--- lib/characterStyles/ScreenTitle.dart
+++ lib/characterStyles/ScreenTitle.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:museum_guide_app/theme/theme.dart';
+import 'package:auto_size_text/auto_size_text.dart';
 
 class ScreenTitle extends StatelessWidget {
   final String _content;
@@ -9,9 +10,11 @@ class ScreenTitle extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(
+      child: AutoSizeText(
         this._content,
         style: TextStyle(fontSize: 56, fontWeight: FontWeight.bold),
+        minFontSize: 38,
+        maxLines: 2,
       ),
       margin: EdgeInsets.only(left: layoutMarginLeft),
     );
